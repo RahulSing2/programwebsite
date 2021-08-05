994f360e39df62da50f73bb4f6139adbf14b20bb
RahulSing2 <88368239+RahulSing2@users.noreply.github.com>
2021-08-05 13:19:28 +0530

    programwebsite

---
+++ programwebsite/programwebsite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace programwebsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i, fact = 1, number;
+            Console.WriteLine("Rahul Singh Hello World!");
+            Console.Write("Enter any Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial of " + number + " is: " + fact);
+            Console.ReadKey();
+        }
+    }
+}
